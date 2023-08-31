d3b23f045e7b6cdadcd1250801f6e4ce93a0bf42
Gaby Markley <gabrielle.markley.505@my.csun.edu.com>
2023-08-31 14:40:37 -0700

    seedData added

---
+++ server/db/seed.js
@@ -0,0 +1,144 @@
+
+
+
+
+
+
+// // THIS FILE WILL RESET YOUR DATABASE - PROCEED WITH CAUTION
+// //pulling in connection to my local database
+// const client = require('./client')
+
+// const { createTrainer, getAllTrainers } = require('./helpers/trainers')
+// const { createType } = require('./helpers/types')
+// const { createSpecies, getSpeciesById } = require('./helpers/species')
+// const { createPokemon } = require('./helpers/pokemon')
+
+// const { trainers, types, species, pokemon } = require('./seedData')
+
+// //Drop Tables for cleanliness
+// const dropTables = async () => {
+//     try {
+//         console.log("Starting to drop tables")
+//         await client.query(`
+//         DROP TABLE IF EXISTS pokemon;
+//         DROP TABLE IF EXISTS species;
+//         DROP TABLE IF EXISTS types;
+//         DROP TABLE IF EXISTS trainers;
+//         `)
+//         console.log("Tables dropped!")
+//     } catch (error) {
+//         console.log("Error dropping tables")
+//         throw error
+//     }
+// }
+
+// //Create Tables because we need a place for the data to live
+// const createTables = async () => {
+//     console.log("Building tables...")
+//     await client.query(`
+//         CREATE TABLE trainers (
+//             "trainerId" SERIAL PRIMARY KEY,
+//             username varchar(255) UNIQUE NOT NULL,
+//             password varchar(255) NOT NULL,
+//             name varchar(255) NOT NULL
+//         );
+//         CREATE TABLE types (
+//             type_id SERIAL PRIMARY KEY,
+//             type varchar(255) UNIQUE NOT NULL
+//         );
+//         CREATE TABLE species (
+//             "speciesId" SERIAL PRIMARY KEY,
+//             name varchar(255) UNIQUE NOT NULL,
+//             "primaryTypeId" INTEGER REFERENCES types(type_id) NOT NULL,
+//             "secondaryTypeId" INTEGER REFERENCES types(type_id)
+//         );
+//         CREATE TABLE pokemon (
+//             "pokemonId" SERIAL PRIMARY KEY,
+//             "speciesId" INTEGER REFERENCES species("speciesId") NOT NULL,
+//             name varchar(255) NOT NULL,
+//             "trainerId" INTEGER REFERENCES trainers("trainerId"),
+//             is_fainted BOOLEAN NOT NULL
+//         );
+//     `)
+//     console.log("Tables built!")
+// }
+
+// //Insert mock data from seedData.js
+// //Create trainers
+// const createInitialTrainers = async () => {
+//     try {
+//         //Looping through the "trainers" array from seedData
+//         for (const trainer of trainers) {
+//             //Insert each trainer into the table
+//             await createTrainer(trainer)
+//         }
+//         console.log("created trainers")
+//     } catch (error) {
+//         throw error
+//     }
+// }
+
+// //Create types
+// const createInitialTypes = async () => {
+//     try {
+//         for (const typeName of types) {
+//             //Structured like this because we only have an array of strings in the seed data, and we want to put that in object format for the function
+//             await createType( { type: typeName } )
+//         }
+//         console.log("created types")
+//     } catch (error) {
+//         throw error
+//     }
+// }
+
+// //Create species
+// const createInitialSpecies = async () => {
+//     try {
+//         for (const specy of species) {
+//             //Single specy because we're popping one at a time in the DB
+//             await createSpecies(specy)
+//         }
+//         console.log("created species")
+//     } catch (error) {
+//         throw error
+//     }
+// }
+
+// //Create pokemon
+// const createInitialPokemon = async () => {
+//     try {
+//         for (const pokeman of pokemon) {
+//             //Single pokeman because we're popping one at a time in the DB
+//             await createPokemon(pokeman)
+//         }
+//         console.log("created pokemon")
+//     } catch (error) {
+//         throw error
+//     }
+// }
+
+// //Call all my functions and 'BUILD' my database
+// const rebuildDb = async () => {
+//     try {
+//         //ACTUALLY connect to my local database
+//         client.connect()
+//         //Run our functions
+//         await dropTables()
+//         await createTables()
+
+//         //Generating starting data
+//         console.log("starting to seed...")
+//         await createInitialTrainers()
+//         await createInitialTypes()
+//         await createInitialSpecies()
+//         await createInitialPokemon()
+
+//     } catch (error) {
+//         console.error(error)
+//     } finally {
+//         //close our connection
+//         client.end()
+//     }
+// }
+
+// rebuildDb()
--- server/db/seedData.js
+++ server/db/seedData.js
@@ -1,15 +1,37 @@
-const client = require ('./client')
+const client = require("./client");
 
 const users = [
-{username: 'jsmith123', password: 'fjdksla;', DOB: '1967/08/21'}
-{username: 'gmarkley456', password: 'lovemovies', DOB: '1995/05/21'}
-{username: 'georgiegirl', password: 'cjkei3!', DOB: '2000/01/05'}
-{username: 'horrormovieluvr', password: 'breakbEetle$', DOB: '1983/09/25'}
-{username: 'cl0720', password: 'fjeisl31!', DOB: '1995/07/20'}
-]
+  { username: "jsmith123", password: "fjdksla;", DOB: "1967/08/21" },
+  { username: "gmarkley456", password: "lovemovies", DOB: "1995/05/21" },
+  { username: "georgiegirl", password: "cjkei3!", DOB: "2000/01/05" },
+  { username: "horrormovieluvr", password: "breakbEetle$", DOB: "1983/09/25" },
+  { username: "cl0720", password: "fjeisl31!", DOB: "1995/07/20" },
+];
 
-// const trainers = [
-//     { username: 'ashrox', password: 'i<3profoak', name: 'Ash Ketchum'},
-//     { username: 'waterwaterwater', password: 'lovestarmi', name: 'Misty LivesAtTheGym'},
-//     { username: 'bestTrainerEvah', password: 'snorlaxLyfe', name: 'Megan Boczar'}
-// ]
+const movies = [
+  {
+    name: "Eternal Sunshine of the Spotless Mind",
+    genre: "drama",
+    image: "https://rb.gy/qbxfq",
+  },
+  { name: "The Orphanage", genre: "horror", image: "https://rb.gy/wn8xg" },
+  { name: "Werewolves Within", genre: "comedy", image: "https://rb.gy/2jahw" },
+  { name: "Parasite", genre: "thriller", image: "https://rb.gy/fbrpg" },
+  {
+    name: "About Time",
+    genre: "sci-fi",
+    image:
+      "https://m.media-amazon.com/images/M/MV5BMTA1ODUzMDA3NzFeQTJeQWpwZ15BbWU3MDgxMTYxNTk@._V1_.jpg",
+  },
+];
+
+const likes = [
+  { user_id: 1, movie_id: 5 },
+  { user_id: 3, movie_id: 5 },
+  { user_id: 2, movie_id: 3 },
+  { user_id: 1, movie_id: 2 },
+  { user_id: 5, movie_id: 2 },
+  { user_id: 4, movie_id: 4 },
+];
+
+module.exports = { users, movies, likes };
