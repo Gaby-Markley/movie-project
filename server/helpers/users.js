93d5868a89920728ab09db39c3a442b697dfc888
Gaby Markley <gabrielle.markley.505@my.csun.edu.com>
2023-09-01 20:14:50 -0700

    created users.js helper functions

---
+++ server/helpers/users.js
@@ -0,0 +1,23 @@
+const client = require("../client");
+
+async function createUser({ username, password, dob }) {
+  try {
+    const {
+      rows: [user],
+      // insert sql query (for each function we created in seed.js
+    } = await client.query(
+      `
+    INSERT INTO users (username, password, dob) 
+    VALUES($1, $2, $3)
+    RETURNING *;
+    `,
+      // similar to a dependency array, which stops an infinite loop
+      [username, password, dob]
+    );
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
+module.exports = { createUser };
