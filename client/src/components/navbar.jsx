60e60dfbe90407c185fb326da391b23d8dbd662c
Gaby Markley <gabrielle.markley.505@my.csun.edu.com>
2023-09-07 15:47:13 -0700

    deleted home and updated navbar

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 export default function NavBar() {
   return (
     <div id="navbar-container">
-      <Link to="/">Home</Link>
       <Link to="/Profile">Profile</Link>
       <Link to="/Movies">Movies</Link>
       <Link to="/LogIn">Log In</Link>
